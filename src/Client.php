c67450e9e10b8461050e5fcd3ce1f45d66b06886
Tomáš Naibrt <tomas@naibrt.cz>
2021-08-19 16:38:07 +0200

    refactoring

--- src/Client.php
+++ src/Client.php
@@ -99,89 +99,55 @@ class Client
         return $this->accessToken;
     }
 
-    public function mutation($method, $data, $query)
+    public function mutation(string $method, array $arguments, array $body): array
     {
-        $builder = new MutationBuilder();
+        return $this->call('mutation', $method, $arguments, $body);
+    }
+
+    public function query(string $method, array $arguments, array $body): array
+    {
+        return $this->call('query', $method, $arguments, $body);
+    }
+
+    public function call(string $type, string $method, array $arguments, array $body): array
+    {
+        $builder = $type == "mutation" ? new MutationBuilder() : new QueryBuilder();
         $readyQuery = $builder
             ->name($method)
-            ->arguments($data)
-            ->body($query)
+            ->arguments($arguments)
+            ->body($body)
             ->build();
 
-
-        $request = $this->requestFactory->createRequest('POST', $this->getEndpointUri('/graphql'));
-        $request->withHeader()
-                ->withBody('xa');
+        $request = $this->requestFactory->createRequest('POST', $this->getEndpointUri('/graphql'))
+            ->withHeader('Content-Type', 'application/json; charset=utf-8')
+            ->withHeader('Accept', 'application/json')
+            ->withHeader('Authorization', 'Bearer ' . $this->accessToken)
+            ->withBody($this->streamFactory->createStream($readyQuery));
 
         $response = $this->httpClient->sendRequest($request);
 
-//        request('POST', $this->getEndpoint('/graphql'), [
-//            'json' => [
-//                'query' => $readyQuery,
-//            ],
-//            'headers' => [
-//                'Authorization' => 'Bearer ' . $this->accessToken,
-//                'Accept' => 'application/json',
-//                'Content-type' => 'application/json; charset=utf-8',
-//            ],
-//            //'debug' => true
-//        ]);
-//
-//        $res = json_decode((string)$res->getBody(), true);
-//
-//        if(isset($res['errors'])){
-//            throw new Exception(json_encode($res['errors']));
-//        }
-//        elseif(isset($res['data'][$method])) {
-//            return $res['data'][$method];
-//        }
-//        else {
-//            return $res;
-//        }
-    }
-    
-    public function query($method, $data, $query)
-    {
-//        $builder = new QueryBuilder();
-//        $readyQuery = $builder
-//            ->name($method)
-//            ->arguments($data)
-//            ->body($query)
-//            ->build();
-//
-//        $res = $this->guzzle->request('POST', $this->getEndpoint('/graphql'), [
-//            'json' => [
-//                'query' => $readyQuery,
-//            ],
-//            'headers' => [
-//                'Authorization' => 'Bearer ' . $this->accessToken,
-//                'Accept' => 'application/json',
-//                'Content-type' => 'application/json; charset=utf-8',
-//            ],
-//            //'debug' => true
-//        ]);
-//
-//        $res = json_decode((string)$res->getBody(), true);
-//
-//        if(isset($res['errors'])){
-//            throw new Exception(json_encode($res['errors']));
-//        }
-//        elseif(isset($res['data'][$method])) {
-//            return $res['data'][$method];
-//        }
-//        else {
-//            return $res;
-//        }
-    }
-
-    protected function getEndpointUri(string $string): string
-    {
-        return $this->baseUrl.$string;
+        if($response->getStatusCode() == 200) {
+            $res = json_decode((string)$response->getBody(), true);
+            if(isset($res['error'])){
+                throw new Exception($res['error']);
+            }
+            elseif(isset($res['errors'])){
+                throw new Exception($res['errors']);
+            }
+            elseif(isset($res['data'][$method])) {
+                return $res['data'][$method];
+            }
+            else {
+                return $res;
+            }
+        }
+        else {
+            dump($response);
+        }
     }
 
     public function authenticate()
     {
-        $request = $this->requestFactory->createRequest('POST', $this->getEndpointUri('/oauth/v2/token'));
         $body = http_build_query([
             'client_id' => $this->clientId,
             'client_secret' => $this->clientSecret,
@@ -189,154 +155,24 @@ class Client
             'username' => $this->username,
             'password' => $this->password,
         ]);
-        var_dump($body);
-        $request
-            ->withHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+        $request = $this->requestFactory->createRequest('POST', $this->getEndpointUri('/oauth/v2/token'))
+            ->withHeader('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8')
             ->withBody($this->streamFactory->createStream($body));
-        var_dump($request);
+
         $response = $this->httpClient->sendRequest($request);
-        var_dump((string)$response->getBody());
-        die();
 
-        $res = $this->guzzle->request('GET', $this->getEndpointUri('/oauth/v2/token'), [
-            'query' => [
-                'client_id' => $this->clientId,
-                'client_secret' => $this->clientSecret,
-                'grant_type' => 'password',
-                'username' => $this->username,
-                'password' => $this->password,
-            ],
-            'headers' => []
-        ]);
+        if($response->getStatusCode() == 200) {
+            $res = json_decode((string)$response->getBody(), true);
+            $this->accessToken = $res['access_token'];
+        }
+        else {
+            dump($response);
+        }
+    }
 
-        $res = json_decode((string)$res->getBody(), true);
-        $this->accessToken = $res['access_token'];
-    }
-    
-    /**
-     * 
-     * @param int $checkoutId
-     * @param int $number
-     * @param string $dateCreated
-     * @param string $currency
-     * @param int $totalPrice
-     * @param int $priceZeroVat
-     * @param int $priceStandardVat
-     * @param int $vatStandard
-     * @param int $priceFirstReducedVat
-     * @param int $vatFirstReduced
-     * @param int $priceSecondReducedVat
-     * @param int $vatSecondReduced
-     * @param int $priceForSubsequentSettlement
-     * @param int $priceUsedSubsequentSettlement
-     * @return array
-     * @throws Exception
-     */
-    public function createEetReceipt($checkoutId, $number, $dateCreated, $currency, $totalPrice,
-            $priceZeroVat = null, $priceStandardVat = null, $vatStandard = null, $priceFirstReducedVat = null,
-            $vatFirstReduced = null, $priceSecondReducedVat = null, $vatSecondReduced = null, 
-            $priceForSubsequentSettlement = null, $priceUsedSubsequentSettlement = null
-            ) 
+    protected function getEndpointUri(string $string): string
     {
-        $data = [
-                    'checkout' => $checkoutId,
-                    'number' => $number,
-                    'dateCreated' => $dateCreated,
-                    'currency' => $currency,
-                    'totalPrice' => $totalPrice,
-                    'priceZeroVat' => $priceZeroVat,
-                    'priceStandardVat' => $priceStandardVat,
-                    'vatStandard' => $vatStandard,
-                    'priceFirstReducedVat' => $priceFirstReducedVat,
-                    'vatFirstReduced' => $vatFirstReduced,
-                    'priceSecondReducedVat' => $priceSecondReducedVat,
-                    'vatSecondReduced' => $vatSecondReduced,
-                    'priceForSubsequentSettlement' => $priceForSubsequentSettlement,
-                    'priceUsedSubsequentSettlement' => $priceUsedSubsequentSettlement,
-                ];
-        
-        foreach($data as $key => $val) {
-            if($data[$key] == null) {
-                unset($data[$key]);
-            }
-        }
-        
-        $res = $this->mutation('createEetReceipt', 
-                $data, 
-                ['data' => $this->getReceiptParams(), 'errors']);
-        
-        if(count($res['errors'])){
-            throw new Exception(json_encode($res['errors']));
-        }
-        
-        return $res;
-    }
-    
-    /**
-     * 
-     * @param int $id
-     * @return array
-     * @throws Exception
-     */
-    public function sendEetReceipt($id) 
-    {
-        $res = $this->mutation('sendEetReceipt', 
-                ['id' => $id], 
-                ['data' => $this->getReceiptParams(), 'errors']);
-        
-        if(count($res['errors'])){
-            throw new Exception(json_encode($res['errors']));
-        }
-        
-        return $res;
-    }
-    
-    /**
-     * 
-     * @param int $page
-     * @param string $sort
-     * @param string $direction asc/desc
-     * @param array $filters
-     * @return array
-     * @throws Exception
-     */
-    public function getEetReceipts($page = 1, $sort = 'id', $direction = 'asc', $filters = []) 
-    {
-        $res = $this->query('eetReceipts', 
-                ['page' => $page, 'sort' => $sort, 'direction' => $direction, 'filters' => $filters], 
-                ['items' => $this->getReceiptParams()]);
-        
-        return $res;
-    }
-    
-    /**
-     * 
-     * @return array
-     */
-    protected function getReceiptParams()
-    {
-        return [
-                    'id',
-                    'dic',
-                    'checkoutEetId',
-                    'shopEetId',
-                    'playground',
-                    'verificationMode',
-                    'number',
-                    'dateCreated',
-                    'totalPrice',
-                    'priceZeroVat',
-                    'priceStandardVat',
-                    'vatStandard',
-                    'priceFirstReducedVat',
-                    'vatFirstReduced',
-                    'priceSecondReducedVat',
-                    'vatSecondReduced',
-                    'priceForSubsequentSettlement',
-                    'priceUsedSubsequentSettlement',
-                    'fik',
-                    'bkp',
-                    'pkp',
-                ];
+        return $this->baseUrl.$string;
     }
 }
